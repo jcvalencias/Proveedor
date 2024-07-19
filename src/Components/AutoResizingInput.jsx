f291cde9e9fd38e3737ebdd23c8bd94393ea44c0
jcvalencias <jcvalencias15@gmail.com>
2024-07-19 06:40:34 -0500

    incluye is being added

--- src/Components/AutoResizingInput.jsx
+++ src/Components/AutoResizingInput.jsx
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AutoResizingInput = () => {
   const [value, setValue] = useState('');
+  const [items, setItems] = useState([]);
 
-  const handleChange = (e) => {
+  const handleTextChange = (e) => {
     setValue(e.target.value);
   };
 
-  console.log(value.length);
+  const  handleSave = () => {
+    setItems([...items, value]);
+  }
+
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
+  
 
   return (
+    <>
     <div className="input-container">
       <input
         type="text"
         value={value}
-        onChange={handleChange}
+        onChange={handleTextChange}
         className="auto-resizing-input"
-        style={{ width: `${Math.max(value.length * 10, 100)}px` }}
-        placeholder="Type here..."
+        style={{ width: `${Math.max(value.length * 6, 10)}px` }}
+        placeholder="|"
       />
+      <button className='btnEdit' onClick={handleSave}></button>
     </div>
+    <div className='Options'>
+      {items.map((item, index)=> (
+      <div className='optionIncluye'>
+        <span>
+          {item}
+        </span>
+        <button className='btnDelete'></button>
+      </div>
+      ))}
+    </div>
+    </>
   );
 };
 
