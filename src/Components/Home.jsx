17b2ea1ed79f9c8782c7a0befd42e438de98cb6a
jcvalencias <jcvalencias15@gmail.com>
2024-07-21 16:38:46 -0500

    setting preuploader

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,40 +1,19 @@
 import { useState } from 'react';
+import '../Site/assets/css/style.css'
+import "../Site/assets/css/default.css"
+import "../Site/assets/css/bootstrap.min.css"
+import "../Site/assets/fonts/themify-icons/themify-icons.css"
+import "../Site/assets/css/magnific-popup.css"
+import "../Site/assets/css/nice-select.css"
+import "../Site/assets/css/jquery-ui.min.css"
+import "../Site/assets/css/animate.css"
+
 
 function Home() {
     return (
 <>
-  {/*====== Required meta tags ======*/}
-  <meta charSet="utf-8" />
-  <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-  <meta name="description" content="" />
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, shrink-to-fit=no"
-  />
-  {/*====== Title ======*/}
-  <title>Solukay - Busca, elige y soluciona lo que sea</title>
   {/*====== Favicon Icon ======*/}
   <link rel="shortcut icon" href="../Site/assets/images/favicon.png" type="image/png" />
-  {/*====== Bootstrap css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/bootstrap.min.css" />
-  {/*====== FontAwesoem css ======*/}
-  <link rel="stylesheet" href="../Site/assets/fonts/themify-icons/themify-icons.css" />
-  {/*====== Flaticon css ======*/}
-  <link rel="stylesheet" href="../Site/assets/fonts/flaticon/flaticon.css" />
-  {/*====== Magnific Popup css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/magnific-popup.css" />
-  {/*====== Slick css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/slick.css" />
-  {/*====== Nice-select css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/nice-select.css" />
-  {/*====== Jquery ui css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/jquery-ui.min.css" />
-  {/*====== Animate css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/animate.css" />
-  {/*====== Default css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/default.css" />
-  {/*====== Style css ======*/}
-  <link rel="stylesheet" href="../Site/assets/css/style.css" />
   {/*====== Start Preloader ======*/}
   <div className="preloader">
     <div className="loader">
