d5252e800fed3220588025f84344d7547fccd86b
Juan <jcvalencias15@gmail.com>
2024-07-26 14:42:09 -0500

    no commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,28 +1,26 @@
 import { useState, useEffect } from 'react';
 import Header from './Header';
 
-
 const Home = () => {
-  const [fade, setfade] = useState('');
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setfade(' fade');
-    }, 1000);
-  });
+  const PreLoader = () => {
+    return (
+      <div className="preloader">
+            <div className="loader">
+                <img src="assets/images/loader.png" alt="loader" />
+            </div>
+        </div>
+    );
+  }
 
   return (
-<>
-  {/*====== Start Preloader ======*/}
-  <div className={"preloader" + fade}>
-    <div className="loader">
-      <img src="/assets/images/loader.png" alt="loader" />
-    </div>
-  </div>
-  <Header />
-</>
+  <>
+    {/*====== Start Preloader ======*/}
+    <PreLoader />
+    <Header />
+  </>
 
-    );
+  );
 }
 
 export default Home;
